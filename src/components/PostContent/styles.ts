89457207360cc46c509c49f877af6b4f679aed00
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-01-23 14:38:59 -0300

    feat: add PostContent UI

---
+++ src/components/PostContent/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 2.5rem 2rem;
+  max-width: 52rem;
+  margin: 0 auto;
+  color: ${({ theme }) => theme.COLORS['base-text']};
+`
