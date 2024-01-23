baab099e02346e6b2fa4a7c94a4db3d65a339f51
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-01-22 21:24:08 -0300

    feat: add search ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Profile } from './components/Profile'
 import { Posts } from './pages/Posts'
 
 export const App = () => {
   return (
     <>
       <Posts />
-      <Profile />
     </>
   )
 }
--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,9 +1,14 @@
 import { Cover } from '../../components/Cover'
+import { Profile } from '../../components/Profile'
+import { Search } from '../../components/Search'
+import { Container } from './styles'
 
 export const Posts = () => {
   return (
-    <>
+    <Container>
       <Cover />
-    </>
+      <Profile />
+      <Search />
+    </Container>
   )
 }
